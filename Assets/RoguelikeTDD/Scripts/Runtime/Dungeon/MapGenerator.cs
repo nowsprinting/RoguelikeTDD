a2b74c52563cfbf86660a59348aa1539fb23fb9b
Koji Hasegawa <hasegawa@hubsys.co.jp>
2023-12-22 05:04:22 +0900

    Add GenerateDungeonMap (integration)

--- Assets/RoguelikeTDD/Scripts/Runtime/Dungeon/MapGenerator.cs
+++ Assets/RoguelikeTDD/Scripts/Runtime/Dungeon/MapGenerator.cs
@@ -1,6 +1,8 @@
 // Copyright (c) 2023 Koji Hasegawa.
 // This software is released under the MIT License.
 
+using System.Collections.Generic;
+
 namespace RoguelikeTDD.Dungeon
 {
     public class MapGenerator
@@ -18,6 +20,76 @@ namespace RoguelikeTDD.Dungeon
                     Map[y][x] = MapChip.Wall;
                 }
             }
+        }
+
+        public void Write(Room[] rooms, Passage[] passages, Door[] doors, Stairs[] stairs)
+        {
+            foreach (var passage in passages)
+            {
+                passage.WriteToMap(Map);
+            }
+
+            foreach (var door in doors)
+            {
+                door.WriteToMap(Map);
+            }
+
+            foreach (var room in rooms)
+            {
+                room.WriteToMap(Map);
+            }
+
+            foreach (var stair in stairs)
+            {
+                stair.WriteToMap(Map);
+            }
+        }
+
+        // すべての通路を生成し、配列で返す
+        private static Passage[] CreateAllPassages(Room[] rooms)
+        {
+            var passages = new List<Passage>();
+            var perimeter = Passage.GetOuterPerimeter(rooms);
+            for (var i = 0; i < perimeter.Length - 1; i++)
+            {
+                passages.Add(new Passage(perimeter[i], perimeter[i + 1]));
+            }
+
+            var centralPassage = Passage.GetRandomCentralPassage(rooms);
+            for (var i = 0; i < centralPassage.Length - 1; i++)
+            {
+                passages.Add(new Passage(centralPassage[i], centralPassage[i + 1]));
+            }
+
+            return passages.ToArray();
+        }
+
+        // すべての部屋のドアを生成し、配列で返す
+        private static Door[] CreateAllDoors(Room[] room, Passage[] passages)
+        {
+            var doors = new List<Door>();
+            foreach (var r in room)
+            {
+                doors.AddRange(Door.CreateDoors(r, passages));
+            }
+
+            return doors.ToArray();
+        }
+
+        /// <summary>
+        /// ダンジョンのマップをランダムに生成して返す
+        /// </summary>
+        /// <returns></returns>
+        public static MapGenerator GenerateDungeonMap()
+        {
+            var map = new MapGenerator(80, 24);
+            var rooms = Room.CreateRooms(map.Map[0].Length, map.Map.Length, 3, 2);
+            var passages = CreateAllPassages(rooms);
+            var doors = CreateAllDoors(rooms, passages);
+            var upStairs = new Stairs(rooms, MapChip.UpStairs);
+            var downStairs = new Stairs(rooms, upStairs.Room, MapChip.DownStairs);
+            map.Write(rooms, passages, doors, new[] { upStairs, downStairs });
+            return map;
         }
     }
 }
--- Assets/RoguelikeTDD/Tests/Runtime/Dungeon/MapGeneratorTest.cs
+++ Assets/RoguelikeTDD/Tests/Runtime/Dungeon/MapGeneratorTest.cs
@@ -3,6 +3,7 @@
 
 using System.Linq;
 using NUnit.Framework;
+using RoguelikeTDD.TestUtils;
 
 namespace RoguelikeTDD.Dungeon
 {
@@ -12,6 +13,7 @@ namespace RoguelikeTDD.Dungeon
         [TestCase(30, 20)]
         public void MapGenerator_指定サイズのマップ配列が生成されること(int width, int height)
         {
+            // Act
             var sut = new MapGenerator(width, height);
 
             // Assert
@@ -22,11 +24,48 @@ namespace RoguelikeTDD.Dungeon
         [Test]
         public void MapGenerator_マップ配列が壁で埋められていること()
         {
+            // Arrange
             int width = 20, height = 10;
+
+            // Act
             var sut = new MapGenerator(width, height);
 
+            // Assert
             var column = Enumerable.Repeat(MapChip.Wall, width).ToArray();
             var expected = Enumerable.Repeat(column, height).ToArray();
+            Assert.That(sut.Map, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Write_マップを構成する要素をマップ配列に書き込めること()
+        {
+            // Arrange
+            var sut = new MapGenerator(width: 10, height: 9);
+            var rooms = new Room[] { new Room(1, 1, 3, 3), new Room(5, 5, 4, 3), };
+            var passages = new Passage[] { new Passage(rooms[0], rooms[1]) };
+            var doors = Door.CreateDoors(rooms[0], passages).Concat(Door.CreateDoors(rooms[1], passages)).ToArray();
+            var stairs = new Stairs[]
+            {
+                new Stairs(new[] { new Room(1, 1, 1, 1) }, MapChip.UpStairs),
+                new Stairs(new[] { new Room(6, 6, 1, 1) }, MapChip.DownStairs),
+            };
+            var expected = MapUtil.Parse(new[]
+            {
+                "WWWWWWWWWW", //
+                "WURRWWWWWW", // 3x3の部屋、(0,0)に登り階段
+                "WRRRDPPPWW", //
+                "WRRRWWWPWW", //
+                "WWWWWWWDWW", //
+                "WWWWWRRRRW", // 4x3の部屋、(1,1)に降り階段
+                "WWWWWRSRRW", //
+                "WWWWWRRRRW", //
+                "WWWWWWWWWW", //
+            });
+
+            // Act
+            sut.Write(rooms, passages, doors, stairs);
+
+            // Assert
             Assert.That(sut.Map, Is.EqualTo(expected));
         }
     }
